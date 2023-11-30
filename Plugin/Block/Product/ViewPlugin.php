b0707cf8a75d77eb1805b9b17c46ec42dcd08cd4
mrobotisback <nicolo.garbin@essesolutions.it>
2023-11-30 13:01:44 +0100

    Create cache hash for custom entity in product block

--- Plugin/Block/Product/ViewPlugin.php
+++ Plugin/Block/Product/ViewPlugin.php
@@ -6,12 +6,23 @@ namespace Smile\CustomEntityProductLink\Plugin\Block\Product;
 
 use Magento\Catalog\Block\Product\View;
 use Smile\CustomEntity\Api\Data\CustomEntityInterface;
+use Smile\CustomEntity\Model\CustomEntity;
+use Magento\Framework\Serialize\Serializer\Json  as Serializer;
 
 /**
  * View product block plugin.
  */
 class ViewPlugin
 {
+
+    private Serializer $serializer;
+
+    public function __construct(
+        Serializer $serializer
+    ) {
+        $this->serializer = $serializer;
+    }
+
     /**
      * Append custom entities identities.
      *
@@ -26,10 +37,17 @@ class ViewPlugin
         $customEntities = $source->getProduct()->getExtensionAttributes()->getCustomEntities();
         if ($customEntities) {
             /** @var CustomEntityInterface $customEntity */
+            $customEntityIdentities = [];
             foreach ($customEntities as $customEntity) {
                 // @codingStandardsIgnoreLine
-                $identities = array_merge($identities, $customEntity->getIdentities());
+                $customEntityIdentities[] = $customEntity->getIdentities();
             }
+
+            $identities[] = sprintf(
+                "%s_%s",
+                CustomEntity::CACHE_TAG,
+                sha1($this->serializer->serialize($customEntityIdentities))
+            );
         }
 
         return $identities;
